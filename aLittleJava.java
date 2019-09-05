0d80432741fb36b857b27a97f69314b74c4c7dd8
risa-o <risa.orsinger@gmail.com>
2019-09-04 22:47:33 -0500

    update

---
+++ aLittleJava.java
@@ -0,0 +1,48 @@
+public class aLittleJava {
+
+	abstract class Seasoning{}
+	class Salt extends Seasoning{}
+	class Pepper extends Seasoning{}
+	class Thyme extends seasoning{}
+	class Sage extends seasoning{}
+
+	abstract class Point{}
+	class CartesianPt extends Point{
+		int x;
+		int y;
+		CartesianPt(int _x, int _y){
+		x = _x;
+		y = _y;
+		}
+	}
+
+	class ManhattanPt extends Point{
+		int x;
+		int y:
+		ManhattanPt(int _x, int _y){
+		x = _x;
+		y = _y;
+		}
+	}
+
+	abstract class Num{}
+	class Zero extends Num{}
+	class OneMoreThan extends Num{
+		Num predecessor;
+		OneMoreThan(Num _p){
+		predecessor = _p;
+		}
+	}
+
+	astract int distanceTo0()
+	int distanceTo(){
+		return (int) Math.sqrt(x*x+y*y);
+	}
+	int distanceTo(){
+		return x + y
+	}
+	public static main(){
+
+	}
+}
+
